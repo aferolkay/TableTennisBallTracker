759dc3a82d2f0296bb2a82cb737a4c9f07cf2762
aferolkay <olkayafer@gmail.com>
2023-03-29 16:17:47 +0300

    calibration finalized: regression parametreleri utilize edildi, test için arayüz oluşturuldu.
    Bu son commitle birlikte, kalibrasyon kısmı bitti. Test edilen değerler gerçekten biraz uzaklar(özellikle x değerleri) , belki daha fazla kalibrasyon noktası gerekebilir (daha iyi bir regression training için).

--- measureTable.py
+++ measureTable.py
@@ -19,7 +19,6 @@ keyPts = np.zeros((8,2))
 iterator = 0
 lastCursor = (0,0)
 
-
 def getPixelInfo(event,x,y,flags,param):
     global iterator
     global keyPts
@@ -31,8 +30,6 @@ def getPixelInfo(event,x,y,flags,param):
         iterator = iterator-1
         keyPts.pop()
     elif event == cv2.EVENT_LBUTTONDOWN:
-        #keyPts.append(0)
-        #keyPts[iterator] = (x,y)
         keyPts[iterator,0] = x
         keyPts[iterator,1] = y
         iterator = iterator+1
@@ -41,31 +38,25 @@ cv2.namedWindow(winname="Calibrate")
 cv2.setMouseCallback("Calibrate",getPixelInfo)
 
 def gradientDescent( pts ):
-    # TO DO: BU KODU KENDINE GORE ADJUST ET    
     x = pts[:,0]
-    print(x.shape)
     y = pts[:,1]
-    print(y.shape)
     X, Y = np.meshgrid(x, y, copy=False)   
-
-
-    # Z = X**2 + Y**2 + np.random.rand(*X.shape)*0.01
-    zx = np.array([0,152,0,152 , 0,152,76,76])
-    zy = np.array([0,0,274,274 , 137,137,102,172])
-    Zx, Zy = np.meshgrid(x, y, copy=False)
-
 
     X = X.flatten()
     Y = Y.flatten()
 
-    A = np.array([X*0+1, X, Y, X**2, X**2*Y, X**2*Y**2, Y**2, X*Y**2, X*Y]).T
+    zx = np.array([0,152,0,152 , 0 , 152,76 , 76])
+    zy = np.array([0,0,274,274 , 137,137,102,172])
+    Zx, Zy = np.meshgrid(zx, zy, copy=False)
+
+    A = np.array( [X*0+1, X, Y, X**2, X**2*Y, X**2*Y**2, Y**2, X*Y**2, X*Y] ).T
     B = Zx.flatten()
     C = Zy.flatten()
 
-    coeff, r, rank, s = np.linalg.lstsq(A, B)
+    coeffx, r, rank, s = np.linalg.lstsq(A, B)
+    coeffy, r, rank, s = np.linalg.lstsq(A, C)
 
-    return coeff
-
+    return coeffx,coeffy
 
 cam = cv2.VideoCapture("resources/tableTennisBall.mp4")
 ret, frame = cam.read()
@@ -73,6 +64,7 @@ if ret == 0:
     print("Can't get the feed")
     exit()
 
+# CALIBRATION WITH USER INPUT
 calibration = True
 while calibration:
     
@@ -88,15 +80,22 @@ while calibration:
     if cv2.waitKey(1)  & 0xFF == 27:
         break
 
-print(keyPts.shape) #(8,2)
 
-coefficients = gradientDescent(keyPts) 
 
+# TEST WHETHER CALCULATIONS ARE CORRECT
+coefficientX,coefficientY = gradientDescent(keyPts) 
 ret, frame = cam.read()
-while calibration:
-    
+while True:
+    X = lastCursor[0]
+    Y = lastCursor[1]
+    cursorMatrix = np.array([X*0+1, X, Y, X**2, X**2*Y, X**2*Y**2, Y**2, X*Y**2, X*Y]).T
+    xLocation = np.matmul( coefficientX , cursorMatrix )
+    yLocation = np.matmul( coefficientY , cursorMatrix )
+    cv2.rectangle( img=frame , pt1=(10,10) , pt2=(600,30), color = (255,255,255), thickness = 30 )  
+    cv2.putText(img=frame , text= str(int(xLocation))+","+str(int(yLocation))      ,org=(10,30),fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 0, 0),thickness=1)
+
     cv2.imshow("Calibrate",frame)
-    if cv2.waitKey(1)  & 0xFF == 27:
+    if cv2.waitKey(10)  & 0xFF == 27:
         break
 
 cam.release()
